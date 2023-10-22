3e5503aa06a9203a76ad7ed6a031253b93b46d11
Ashish <ashishrajput15@gmail.com>
2023-10-22 12:27:16 +0530

    magic form

---
+++ background.js
@@ -0,0 +1,34 @@
+// event to run execute.js content when extension's button is clicked
+chrome.action.onClicked.addListener(execScript);
+
+async function execScript() {
+  const tabId = await getTabId();
+  chrome.scripting.executeScript({
+    target: {tabId: tabId},
+    files: ['execute.js']
+  });
+  chrome.scripting.executeScript({
+    target: {tabId: tabId},
+    files: ['after.js']
+  });
+}
+
+async function getTabId() {
+  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  return (tabs.length > 0) ? tabs[0].id : null;
+}
+
+// background.js
+let storedSentences = [];
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'storeSentences') {
+    storedSentences = message.data;
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'getStoredSentences') {
+    sendResponse(storedSentences);
+  }
+});
